4cf183d026cf68fa52920e5f8ca60b5c5dd00fa0
onair-lena <pechenko@bk.ru>
2023-07-14 13:26:29 +0300

    Isomorphic Strings

---
+++ isomorphic-strings.js
@@ -0,0 +1,11 @@
+//205. Isomorphic Strings
+
+var isIsomorphic = function(s, t) {
+      if (s.length !== t.length) return false;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) !== t.indexOf(t[i])) return false;
+  }
+
+  return true;
+};
